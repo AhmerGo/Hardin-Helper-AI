3b33bbbc25d81b2ed25c71551e01d796b5cbf28f
Jaden Barnwell <24jbarnwell@gmail.com>
2024-03-05 15:22:47 -0600

    Integrated database file with backend and added find users method that needs to connect to frontend now

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -5,6 +5,8 @@ from langchain_community.embeddings import LlamaCppEmbeddings
 from langchain_community.llms import GPT4All
 from langchain.vectorstores.faiss import FAISS
 from langchain.chains import ConversationalRetrievalChain
+from database_helper import Connection
+
 
 # Modify the system path to be able to import HSU from a different directory
 import sys
@@ -18,6 +20,7 @@ CORS(app, resources={r"/chat": {"origins": "*"}})
 
 # Instantiate your HSU class
 hsu = HSU()
+
 
 @app.route('/')
 def index():
@@ -38,6 +41,20 @@ def chat():
     except Exception as e:
         print(e)  # For development only, use logging in production
         return jsonify({'error': 'Internal Server Error'}), 500
+
+#Database interaction
+#initiate connection
+db_connection = Connection()
+db_connection.connect("admin", "Stevencantremember", "admin")
+
+@app.route('/find_users', methods=['GET'])
+def find_users():
+    users = db_connection.read("chatbot", "users")
+
+    if users:
+        return jsonify({'users': users}), 200
+    return jsonify({'error': 'User not found'}), 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
