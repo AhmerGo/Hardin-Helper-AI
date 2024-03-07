410bcbfd835fa9929d467a3a80c86f5c203e33ff
Dalton <dsm2103@hsutx.edu>
2024-03-06 22:00:40 -0600

    Fixed broken ui

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -62,8 +62,6 @@ function ChatBot() {
   }
 
 
-  function saveChat(){
-=======
   function saveChat() {
     const userInputs = document.querySelectorAll(".message.user");
     const botInputs = document.querySelectorAll(".message.bot");
@@ -130,46 +128,7 @@ function ChatBot() {
             Save Chat
           </button>
         </div>
-    <div className="chat-section flex flex-col h-96 bg-purple rounded-xl shadow-2xl p-6">
-      <div
-        className="flex-grow overflow-auto mb-4 p-4 bg-white rounded-xl shadow-inner"
-        id="chatHistory"
-      >
-        {chatHistory.map((chat, index) => (
-          <div key={index} className={`message ${chat.sender}`}>
-            {chat.text}
-          </div>
-        ))}
-      </div>
-      <div className="flex border-t border-gray-200 pt-4">
-        <textarea
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className="flex-grow p-3 border border-gray-300 rounded-l-xl focus:outline-none  focus:border-transparent"
-          placeholder="Type your message..."
-          style={{ resize: "none" }}
-        />
-        <button
-          onClick={sendMessage}
-          className="bg-[#401486] text-white p-3 rounded-r-xl shadow-md hover:bg-purple-700  focus:ring-4 focus:ring-purple-500 focus:ring-offset-2 transition duration-300 ease-in-out transform hover:text-gold hover:scale-105"
-        >
-          Send
-        </button>
-      </div>
-      <div className="flex justify-evenly my-2.5">
-        <button
-          onClick={clearChat}
-          className="bg-[#401486] text-white p-3 rounded-xl shadow-md focus:outline-none focus:ring-4 focus:ring-purple-500 focus:ring-offset-2 transition duration-300 ease-in-out transform hover:text-gold transform hover:scale-105"
-        >
-          Clear Chat
-        </button>
-        <button
-          onClick={saveChat}
-          className="bg-[#401486] text-white p-3 rounded-xl shadow-md focus:outline-none focus:ring-4 focus:ring-purple-500 focus:ring-offset-2 transition duration-300 ease-in-out transform hover:text-gold transform hover:scale-105"
-        >
-          Save Chat
-        </button>
-      </div>
+        </div>
   );
 }
 
