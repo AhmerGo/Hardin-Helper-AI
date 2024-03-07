dd78cf3a495b0d22ea928b11fdb0965e92ea7d8a
Ahmer Gondal <gondal.ahmer@yahoo.com>
2024-03-06 23:50:35 -0600

    GPU experimentation

--- LLM/HSU.py
+++ LLM/HSU.py
@@ -13,7 +13,7 @@ class HSU:
         
         embeddings = LlamaCppEmbeddings(model_path=model_path)
         index = FAISS.load_local(index_path, embeddings, allow_dangerous_deserialization=True)
-        llm = GPT4All(model=model_path)
+        llm = GPT4All(model=model_path, gpu_layers=20)
         qa = ConversationalRetrievalChain.from_llm(
             llm=llm,
             retriever=index.as_retriever(),
