85121a57063806d56d4393fbc1c918e255a2aed6
Ahmer Gondal <gondal.ahmer@yahoo.com>
2024-03-06 23:54:53 -0600

    Test

--- LLM/HSU.py
+++ LLM/HSU.py
@@ -2,18 +2,20 @@ from langchain_community.embeddings import LlamaCppEmbeddings
 from langchain_community.llms import GPT4All
 from langchain.vectorstores.faiss import FAISS
 from langchain.chains import ConversationalRetrievalChain
-
-
+import torch
 
 class HSU:
     def rag(question):
-        
         model_path = "../LLM/Models/mistral-7b-openorca.Q4_0.gguf"
         index_path = "../LLM/HSU_index"
         
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Using device: {device}")
+        
         embeddings = LlamaCppEmbeddings(model_path=model_path)
         index = FAISS.load_local(index_path, embeddings, allow_dangerous_deserialization=True)
-        llm = GPT4All(model=model_path, gpu_layers=20)
+        llm = GPT4All(model=model_path, device=device)
+        
         qa = ConversationalRetrievalChain.from_llm(
             llm=llm,
             retriever=index.as_retriever(),
@@ -21,6 +23,7 @@ class HSU:
             verbose=True,
             max_tokens_limit=1000,
         )
+        
         # Chatbot loop
         chat_history = []
         result = qa({"question": question, "chat_history": chat_history})
