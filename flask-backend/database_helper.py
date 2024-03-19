6862a99aa0157ce608a5697580c81abc8659dc9a
Steven Barnas <steven.a.barnas@hsutx.edu>
2024-03-19 15:19:36 -0500

    Fixed delete methods - it now works with ObjectID parameter

--- flask-backend/database_helper.py
+++ flask-backend/database_helper.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 """
 Helper file for HossBot MongoDB database operations.
@@ -46,17 +47,6 @@ class Connection(object):
             print(document)
 
     """
-    Inserts a new document into the specified collection.
-    @param db_name: Name of the MongoDB database to access (String)
-    @param collection_name: Name of the MongoDB collection to access (String)
-    @param document: Document to be inserted into the collection (Dictionary)
-    """
-    def create(self, db_name, collection_name, document):
-        db = self.client[db_name]
-        collection = db[collection_name]
-        collection.insert_one(document)
-    """
-
     Inserts one user into the user table after checking for duplicates
     @param name: Name of user (String)
     @param email: Email of user (String)
@@ -86,17 +76,18 @@ class Connection(object):
 
     """
     Inserts a chat log session into the chatlog table.
+    @param user_id: User ID of user that generated the chat log (Int)
     @param chat_log: Chat log that's being saved (String)
     @param response_flag_1: User reports as good answer (Int)
     @param response_flag_2: User reports as incorrect/low quality answer (Int)
     @param response_flag_3: User reports as inappropriate answer (Int)
     @param save_flag: Flags that chat was saved by user (Int)
     """
-    def insert_chat_log(self, chat_log, response_flag_1, response_flag_2, response_flag_3, save_flag):
+    def insert_chat_log(self, user_id, chat_log, response_flag_1, response_flag_2, response_flag_3, save_flag):
         db = self.client["chatbot"]
         collection = db["chatlog"]
         collection.insert_one(
-            {'chat_log': chat_log,
+            {'user_id': user_id, 'chat_log': chat_log,
              'flags': {'response_flag_1': response_flag_1, 'response_flag_2': response_flag_2,
                        'response_flag_3': response_flag_3, 'save_flag': save_flag}})
 
@@ -188,7 +179,7 @@ class Connection(object):
     def delete_users(self, object_id):
         db = self.client["chatbot"]
         collection = db["users"]
-        collection.delete_one({'_id': object_id})
+        collection.delete_one({'_id': ObjectId(object_id)})
 
     """
     Delete a chat log from the chatlog table
@@ -197,7 +188,7 @@ class Connection(object):
     def delete_chat_log(self, object_id):
         db = self.client["chatbot"]
         collection = db["chatlog"]
-        collection.delete_one({'_id': object_id})
+        collection.delete_one({'_id': ObjectId(object_id)})
 
     """
     Delete a link from the links table
@@ -206,7 +197,7 @@ class Connection(object):
     def delete_link(self, object_id):
         db = self.client["chatbot"]
         collection = db["links"]
-        collection.delete_one({'_id': object_id})
+        collection.delete_one({'_id': ObjectId(object_id)})
 
     """
     Delete a pre-made question from the premade_questions table
@@ -215,7 +206,7 @@ class Connection(object):
     def delete_question(self, object_id):
         db = self.client["chatbot"]
         collection = db["premade_questions"]
-        collection.delete_one({'_id': object_id})
+        collection.delete_one({'_id': ObjectId(object_id)})
 
     """
     Closes connection to MongoDB
@@ -226,17 +217,20 @@ class Connection(object):
 
 # test = Connection()
 # Connection.connect(test, "admin", "Stevencantremember", "admin")
-# # Connection.insert_users(test, 2, "Steven Barnas", "steven@hsutx.edu")
-# # Connection.insert_links(test, "www.hsutx.edu")
-# # Connection.update_tally(test, "www.hsutx.edu")
-# # Connection.read(test,"chatbot", "users")
-# # Connection.read(test,"chatbot", "links")
-# # Connection.read(test,"chatbot", "chatlog")
-# # print(Connection.find_tally(test, "www.hsutx.edu"))
-# # print(Connection.find_link(test, "www.hsutx.edu"))
-# # print(Connection.test_object(test, "Steven Barnas"))
-# # Connection.delete_users(test, 2, "Steven Barnas", "steven@hsutx.edu")
-#
+# Connection.insert_users(test, 2, "Steven Barnas", "steven@hsutx.edu")
+# Connection.insert_links(test, "www.hsutx.edu")
+# Connection.update_tally(test, "www.hsutx.edu")
+# Connection.read(test,"chatbot", "users")
+# Connection.read(test,"chatbot", "links")
+# Connection.delete_chat_log(test, '65d7863a3a91bedf9928cf8f')
+# Connection.read(test,"chatbot", "chatlog")
+# print(Connection.find_tally(test, "www.hsutx.edu"))
+# Connection.insert_chat_log(test, 1, "Lorem Ipsum 2", 0, 0, 0, 1)
+# print(Connection.find_link(test, "www.hsutx.edu"))
+# print(Connection.test_object(test, "Steven Barnas"))
+# Connection.delete_users(test, 2, "Steven Barnas", "steven@hsutx.edu")
+# Connection.read(test,"chatbot", "users")
+# print(jsonify(Connection.read(test,"chatbot", "users")))
 # Connection.close(test)
-#
-# # upd = users.update_one({'user_id': 1}, {'$set':{'name': 'Jaden Barnwell', 'email': 'jabster@hsutx.edu'}})
+
+# upd = users.update_one({'user_id': 1}, {'$set':{'name': 'Jaden Barnwell', 'email': 'jabster@hsutx.edu'}})
