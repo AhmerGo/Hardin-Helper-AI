8a8e0597b66fac6657431fc733447589afe763c6
Ahmer Gondal <gondal.ahmer@yahoo.com>
2024-03-28 15:50:29 -0500

    Terminate Script Update

--- terminate.sh
+++ terminate.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Kill the Flask backend process running in the tmux session
-tmux kill-session -t my_session
+flask_pid=$(lsof -t -i :5000)
+if [ -n "$flask_pid" ]; then
+  kill "$flask_pid"
+  echo "Flask process running on port 5000 (PID: $flask_pid) has been terminated."
+else
+  echo "No running Flask process found on port 5000."
+fi
+
 
 # Find the PID of the Node.js process running on port 3000
 node_pid=$(lsof -t -i :3000)
