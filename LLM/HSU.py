59c25c637c370411c4c61ac02a04c1c1d1ccd879
criostasis <criostasis@gmail.com>
2024-03-28 16:04:27 -0500

    Updated the HSU class to include logging for errors and to check if using the gpu. Also got memory working with HSU_chatbot_with_memory, streaming is still a work in progress. I also redid the embeddings and vector store with progams and financial aid websites included. Still working on chunk size and overlap to see what produces the best results. Need to work on the data and create QA pairs from it and test from that to see if we get better answers.

--- LLM/HSU.py
+++ LLM/HSU.py
@@ -1,3 +1,5 @@
+import logging
+import torch
 from langchain_community.embeddings import LlamaCppEmbeddings
 from langchain_community.llms import GPT4All
 from langchain.vectorstores.faiss import FAISS
@@ -5,13 +7,21 @@ from langchain.chains import ConversationalRetrievalChain
 
 
 class HSU:
+    logging.basicConfig(filename='chatbot.log', level=logging.DEBUG, format='%(asctime)s : %(levelname)s : %(message)s')
+
     @staticmethod
     def rag(question):
         model_path = "./Models/wizardlm-13b-v1.2.Q4_0.gguf"
         index_path = "./HSU_index"
         embeddings = LlamaCppEmbeddings(model_path=model_path)
         index = FAISS.load_local(index_path, embeddings)
-        llm = GPT4All(model=model_path, device='gpu')
+        device = "gpu" if torch.cuda.is_available() else "cpu"
+        try:
+            llm = GPT4All(model=model_path, device=device)
+        except Exception as e:
+            logging.error(e, exc_info=True)
+            llm = GPT4All(model=model_path)
+        # llm = GPT4All(model=model_path, device='gpu')
         qa = ConversationalRetrievalChain.from_llm(
             llm=llm,
             retriever=index.as_retriever(),
@@ -22,5 +32,9 @@ class HSU:
 
         # Chatbot loop
         chat_history = []
-        result = qa({"question": question, "chat_history": chat_history})
-        return result
+        try:
+            result = qa({"question": question, "chat_history": chat_history})
+            return result
+        except Exception as e:
+            logging.error(e, exc_info=True)
+        # return result
