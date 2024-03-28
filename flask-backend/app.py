46076c364a68889f5afc6e065eec034b115fd107
Ahmer Gondal <gondal.ahmer@yahoo.com>
2024-03-28 16:47:08 -0500

    debug

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -44,7 +44,7 @@ def chat():
         return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 # #Database interaction
 # #initiate connection
